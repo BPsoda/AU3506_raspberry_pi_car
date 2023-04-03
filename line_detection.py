2e99423120be3efd7d7381ee66b8d98c809cc007
BPsoda <bpsoda@163.com>
2023-04-03 14:19:54 +0800

    Modify roi in line_detection.py.

--- line_detection.py
+++ line_detection.py
@@ -7,8 +7,10 @@ def line_detection(view):
     '''detect the bias between lane and center of the image.
     Please make sure no crossing in the image.'''
     H, W, C = view.shape
-    roi = [1/2, 1/5, 1/3, 1/3]  # region of interest, described in fraction [top, bottom, left, right]
+    roi = [3/5, 1/5, 1/3, 1/3]  # region of interest, described in fraction [top, bottom, left, right]
     crop_img = view[int(roi[0]*H):int((1-roi[1])*H), int(roi[2]*W):int((1-roi[3])*W)]
+    # cv2.imshow('roi', crop_img)
+    # cv2.waitKey(0)
     height, width, _ = crop_img.shape
     hsv = cv2.cvtColor(crop_img, cv2.COLOR_BGR2HSV)
     mask = cv2.inRange(hsv, COLOR_THRESH['black']['lower'], COLOR_THRESH['black']['upper'])
@@ -22,5 +24,5 @@ def line_detection(view):
     return error_x
 
 if __name__ == '__main__':
-    view = cv2.imread('../example/straight.jpg')
+    view = cv2.imread('../example/turn_left.jpg')
     print(line_detection(view))
