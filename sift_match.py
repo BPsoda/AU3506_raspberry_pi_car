5c36854980085f6f0a04aa31176a335c873f0ba6
MoriiHuang <miaochaoe9102@163.com>
2023-04-24 14:52:20 +0800

    add multithread

--- sift_match.py
+++ sift_match.py
@@ -1,49 +1,68 @@
+#coding=utf-8
 import cv2
 import time
 from baseImage import Image
-from image_registration.matching import SIFT
+from image_registration.matching import ORB
+import concurrent.futures
+from functools import lru_cache
 
+# 定义全局变量
+SEARCH_MODULES = [
+    Image('img/test_left_module.png'),
+    Image('img/test_right_module.png'),
+    Image('img/test_goal_module.png'),
+    Image('img/test_left_array_module.png'),
+    Image('img/test_right_array_module.png')
+]
+SEARCH_NAMES = ['左', '右', '靶子', '左箭头', '右箭头']
+
+# 定义ORB匹配器
+MATCHER = ORB(threshold=0.9, rgb=True, nfeatures=50000)
+
+@lru_cache(maxsize=None)
+def search_module(im_source):
+    # 多线程处理，同时搜索多个图像
+    with concurrent.futures.ThreadPoolExecutor() as executor:
+        futures = [executor.submit(MATCHER.find_all_results, im_source, module) for module in SEARCH_MODULES]
+
+    result_with_name = []
+    for future, name in zip(futures, SEARCH_NAMES):
+        result = future.result()
+        if len(result) > 0:
+            if (result[0]['confidence']>0.9):
+                result_with_name.append([name, result[0]])
+                break
+            # result_with_name.append([name, result[0]])
+
+    return result_with_name
+
+@lru_cache(maxsize=None)
 def SIFT_Detection(inputArray):
-    match=SIFT(threshold=0.9, rgb=True, nfeatures=50000)
-    im_source = Image.clone(inputArray)
-    im_search_left= Image('test_left_module.png')
-    im_search_right= Image('test_right_module.png')
-    im_search_goal= Image('test_goal_module.png')
-    im_search_left_array= Image('test_left_array_module.png')
-    im_search_right_array=Image('test_right_array_module.png')
-    search_modules=[im_search_left,im_search_right,im_search_goal,im_search_left_array,im_search_right_array]
-    search_names=['左','右','靶子','左箭头','右箭头']
+    im_source = Image(inputArray)
     start = time.time()
-    result_with_name=[]
-    for module,name in zip(search_modules,search_names):
-        result = match.find_all_results(im_source, module)
-        if (len(result)>0):
-            result_with_name.append([name,result[0]])
+    result_with_name = search_module(im_source)
     img = im_source.clone()
-    if (len(result_with_name)==0):
-        return "没有对应的匹配",img
+    if len(result_with_name) == 0:
+        return "没有对应的匹配", img
     sorted_result = sorted(result_with_name, key=lambda x: x[1]['confidence'])
-    info=f"发现了{sorted_result[0][0]}标识符,匹配相似度:{sorted_result[0][1]['confidence']}"
-    print("use:",time.time() - start,"s")
+    info = "发现了{}标识符,匹配相似度:{}".format(sorted_result[0][0], sorted_result[0][1]['confidence'])
+
     img.rectangle(rect=sorted_result[0][1]['rect'], color=(0, 0, 255), thickness=3)
-    return info,img
 
-if __name__=="__main__":
-    # match = SIFT()
-    im_source = Image('test_goal.png')
-    im_search = Image('test_left_module.png')
+    print("use:", time.time() - start, "s")
+    return info, img
 
-    # start = time.time()
-    # result = match.find_all_results(im_source, im_search)
-    # print(time.time() - start)
-    # print(result)
-    # img = im_source.clone()
-    # for _ in result:
-    #     img.rectangle(rect=_['rect'], color=(0, 0, 255), thickness=3)
-    # img.imshow('ret')
+if __name__ == "__main__":
+    im_source = Image('img/test_goal.png')
+    im_source2 = Image('img/test_array_left.png')
+    for _ in range(3):
+        info, img = SIFT_Detection(im_source2)
+        print(info)
+    for _ in range(2):
+        info, img = SIFT_Detection(im_source)
+        print(info)
+    for _ in range(3):
+        info, img = SIFT_Detection(im_source2)
+        print(info)
+    # img.imshow('img', img)
     # cv2.waitKey(0)
-    info,img=SIFT_Detection(im_source)
-    print(info)
-    # img.imshow('img',img)
-    # cv2.waitKey(0)
-    
